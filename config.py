58ee151b5e2895f394c8b6e0b02174a11aa97ad0
amitchouhan13 <amitchouhan36606@gmail.com>
2025-07-07 12:03:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+AUTHORIZATION_TOKEN = "Bearer your_token_here"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+REFERER = "https://classplusapp.com"
+
+SIGNED_URL_API = "https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url="
+DOWNLOAD_FOLDER = "downloads"
